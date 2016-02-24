add6b15de2687f6ff1ed28f604fd3bbcc9509f18
JohnShekleton <jts15shek@gmail.com>
2016-02-23 23:42:37 -0600

    Create createMenu.py

---
+++ createMenu.py
@@ -0,0 +1,10 @@
+def createMenu(optionlist):
+  '''Description:  Returns printable sting of options
+     Prescription: optionlist is a list of options w/out numbers
+  '''
+  ct = 1
+  optionstr = ''
+  for a in optionlist:
+    optionstr = optionstr + str(ct) + ':' + ' ' + a + '\n'
+    ct += 1
+  return optionstr
